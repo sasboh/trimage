a446c74702451810c34ade6d28b9f308bb750307
Sascha Bohrloch <sasboh@pm.me>
2023-03-21 16:16:43 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# trimage
+
+### resizing, optimizing and converting images to jpg/webp
+
+This simple tool takes any kind of images that you get from marketing or UX and converts it into lean, web-friendly formats jpg and webp. You can resize it by defining a width or height, otherwise it keeps the current size. If you define both, width and height, it crops the image at top/bottom or right/left if necessary. The new files will be renamed in original file name plus new size: _myImage_1024x600.jpg_. The quality level is 80 and the jpg files are progressive.
+
+## Install
+
+```
+$ npm install -g @scout24ch/trimage
+```
+
+## Usage
+
+```
+$ trimage --help
+
+  Usage
+    $ trimage <files>
+
+  Options
+    $ --width, -w  width in pixel, keeps aspect ratio if height is undefined
+    $ --height, -h  height in pixel, keeps aspect ratio if width is undefined
+
+  Examples
+    $ trimage img1
+    $ trimage img1 -w 1024
+    $ trimage img1 img2 -h 600
+    $ trimage img1 img2 img3 -w 1024 -h 600
+
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+const meow = require('meow')
+const chalk = require('chalk')
+const fs = require('fs')
+const sharp = require('sharp')
+
+const errorMsg = chalk.redBright
+const successMsg = chalk.greenBright
+const infoMsg = chalk.blueBright
+
+const cli = meow(
+  `
+  Usage
+    $ trimage <files>
+
+  Options
+    $ --width, -w  width in pixel, keeps aspect ratio if height is undefined
+    $ --height, -h  height in pixel, keeps aspect ratio if width is undefined
+
+  Examples
+    $ trimage img1
+    $ trimage img1 -w 1024
+    $ trimage img1 img2 -h 600
+    $ trimage img1 img2 img3 -w 1024 -h 600
+`,
+  {
+    flags: {
+      width: {
+        type: 'number',
+        alias: 'w'
+      },
+      height: {
+        type: 'number',
+        alias: 'h'
+      }
+    }
+  }
+)
+
+const resize = (file, format, options) => {
+  const filename = file.split('.').shift()
+  return new Promise(resolve => {
+    sharp(file)
+      .toFormat(format, {
+        quality: 80,
+        progressive: true
+      })
+      .resize({
+        width: options.width,
+        height: options.height
+      })
+      .toBuffer({ resolveWithObject: true })
+      .then(({ data, info }) => {
+        const outputFile =
+          filename + '_' + info.width + 'x' + info.height + '.' + format
+        fs.writeFile(outputFile, data, function (err) {
+          if (err) {
+            return console.error(errorMsg(err))
+            process.exit(1)
+          }
+          console.log(file + ' -> ' + outputFile)
+          resolve()
+        })
+      })
+      .catch(err => {
+        console.error(errorMsg(err))
+        process.exit(1)
+      })
+  })
+}
+
+if (cli.input.length === 0 && process.stdin.isTTY) {
+  console.error(errorMsg('Specify at least one file!'))
+  process.exit(1)
+}
+
+console.log(infoMsg('Starting to process ' + cli.input.length + ' file(s)'))
+
+cli.input.forEach(file => {
+  Promise.all([resize(file, 'jpg', cli.flags), resize(file, 'webp', cli.flags)])
+    .then(() => console.log(successMsg(file + ' done')))
+    .catch(err => {
+      console.error(errorMsg(err))
+      process.exit(1)
+    })
+})
